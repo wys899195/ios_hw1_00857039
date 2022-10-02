39661b7bb66b6628ce225213838b3ca68685206b
wys899195 <wys899195@gmail.com>
2022-10-02 21:48:28 +0800

    Initial Commit

---
+++ ios_hw1_00857039/ios_hw1_00857039App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hw1_00857039App.swift
+//  ios_hw1_00857039
+//
+//  Created by User20 on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct ios_hw1_00857039App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
